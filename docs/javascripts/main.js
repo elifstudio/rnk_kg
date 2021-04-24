12c4e9a75a8a6fca4574a2dfb28d6776c346e915
Yryskul <mail.iriskul@gmail.com>
2021-04-24 20:58:40 +0300

    popup

--- docs/javascripts/main.js
+++ docs/javascripts/main.js
@@ -1,8 +1,25 @@
 removeMadeWith();
+modalCloseEvent();
 
 function removeMadeWith() {
     let madeWith = document.getElementsByClassName("md-footer-meta md-typeset");
     if (madeWith[0]) {
         madeWith[0].remove();
+    }
+}
+
+function showModalView(value) {
+    document.getElementById("modal-content-id").innerHTML = value;
+    document.getElementById("modalId").style.display = "block";
+}
+
+function modalCloseEvent() {
+    let modal = document.getElementById("modalId");
+    let close = document.getElementById("closeModalId");
+    close.onclick = function () {
+        modal.style.display = "none";
+    }
+    window.onclick = function (event) {
+        if (event.target == modal) modal.style.display = "none";
     }
 }
