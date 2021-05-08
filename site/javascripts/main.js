82cad32976f65258be2bae71221e7ae76bd0cb07
Yryskul <mail.iriskul@gmail.com>
2021-05-08 14:47:57 +0300

    static

---
+++ site/javascripts/main.js
@@ -0,0 +1,17 @@
+modalCloseEvent();
+
+function showModalView(value) {
+    document.getElementById("modal-content-id").innerHTML = value;
+    document.getElementById("modalId").style.display = "block";
+}
+
+function modalCloseEvent() {
+    let modal = document.getElementById("modalId");
+    let close = document.getElementById("closeModalId");
+    close.onclick = function () {
+        modal.style.display = "none";
+    }
+    window.onclick = function (event) {
+        if (event.target == modal) modal.style.display = "none";
+    }
+}
